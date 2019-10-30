746d52400b38c3c53d74eda723ca87ad15b4e84a
yamgeboers <yamgeboers@han.nl>
2019-10-30 14:31:03 +0100

    version 0.0.1: electric boogaloo

--- Mini-to-Life/Mini-to-Life/Form1.Designer.cs
+++ Mini-to-Life/Mini-to-Life/Form1.Designer.cs
@@ -28,10 +28,30 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 26F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.AutoScroll = true;
+            this.AutoScrollMinSize = new System.Drawing.Size(1920, 1080);
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(1370, 749);
+            this.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.Font = new System.Drawing.Font("Mistral", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(2555, 2121);
+            this.MinimizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(955, 726);
+            this.Name = "Form1";
+            this.Text = "Mini to Life";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.ResumeLayout(false);
+
         }
 
         #endregion
