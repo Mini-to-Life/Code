c8968b87bb02e06b2210c8b9de7705c041e57de8
yamgeboers <yamgeboers@han.nl>
2019-10-31 13:51:02 +0100

    Version 0.2

---
+++ Mini-to-Life/Mini-to-Life/Mini-to-Life_GameScreen.Designer.cs
@@ -0,0 +1,342 @@
+﻿namespace Mini_to_Life
+{
+    partial class MiniToLife
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MiniToLife));
+            this.PB_logo = new System.Windows.Forms.PictureBox();
+            this.Player1 = new System.Windows.Forms.GroupBox();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.TB_playerNaam1 = new System.Windows.Forms.TextBox();
+            this.TB_naam_player1 = new System.Windows.Forms.TextBox();
+            this.PB_player1 = new System.Windows.Forms.PictureBox();
+            this.Player2 = new System.Windows.Forms.GroupBox();
+            this.TB_playerNaam2 = new System.Windows.Forms.TextBox();
+            this.TB_naam_player2 = new System.Windows.Forms.TextBox();
+            this.PB_player2 = new System.Windows.Forms.PictureBox();
+            this.tSTB_player1_Stats = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.tSTB_player1_Buffs = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripSeparator2 = new System.Windows.Forms.ToolStripSeparator();
+            this.tSTB_player1_Rules = new System.Windows.Forms.ToolStripLabel();
+            this.toolStrip2 = new System.Windows.Forms.ToolStrip();
+            this.toolStripLabel1 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripSeparator3 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripLabel2 = new System.Windows.Forms.ToolStripLabel();
+            this.toolStripSeparator4 = new System.Windows.Forms.ToolStripSeparator();
+            this.toolStripLabel3 = new System.Windows.Forms.ToolStripLabel();
+            this.CB_Modelselector_player1 = new System.Windows.Forms.ComboBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.BoxItemRulings_player2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.PB_logo)).BeginInit();
+            this.Player1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PB_player1)).BeginInit();
+            this.Player2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PB_player2)).BeginInit();
+            this.toolStrip2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // PB_logo
+            // 
+            this.PB_logo.Image = ((System.Drawing.Image)(resources.GetObject("PB_logo.Image")));
+            this.PB_logo.Location = new System.Drawing.Point(576, 10);
+            this.PB_logo.Name = "PB_logo";
+            this.PB_logo.Size = new System.Drawing.Size(200, 200);
+            this.PB_logo.TabIndex = 0;
+            this.PB_logo.TabStop = false;
+            // 
+            // Player1
+            // 
+            this.Player1.Controls.Add(this.textBox1);
+            this.Player1.Controls.Add(this.CB_Modelselector_player1);
+            this.Player1.Controls.Add(this.toolStrip1);
+            this.Player1.Controls.Add(this.TB_playerNaam1);
+            this.Player1.Controls.Add(this.TB_naam_player1);
+            this.Player1.Controls.Add(this.PB_player1);
+            this.Player1.Location = new System.Drawing.Point(13, 10);
+            this.Player1.Name = "Player1";
+            this.Player1.Size = new System.Drawing.Size(550, 700);
+            this.Player1.TabIndex = 1;
+            this.Player1.TabStop = false;
+            this.Player1.Text = "Player 1";
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.None;
+            this.toolStrip1.Font = new System.Drawing.Font("Imperial One", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tSTB_player1_Stats,
+            this.toolStripSeparator1,
+            this.tSTB_player1_Buffs,
+            this.toolStripSeparator2,
+            this.tSTB_player1_Rules});
+            this.toolStrip1.Location = new System.Drawing.Point(213, 118);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(177, 25);
+            this.toolStrip1.TabIndex = 3;
+            this.toolStrip1.Text = "TS_player1";
+            // 
+            // TB_playerNaam1
+            // 
+            this.TB_playerNaam1.Location = new System.Drawing.Point(320, 40);
+            this.TB_playerNaam1.Name = "TB_playerNaam1";
+            this.TB_playerNaam1.ReadOnly = true;
+            this.TB_playerNaam1.Size = new System.Drawing.Size(223, 34);
+            this.TB_playerNaam1.TabIndex = 2;
+            // 
+            // TB_naam_player1
+            // 
+            this.TB_naam_player1.Location = new System.Drawing.Point(213, 40);
+            this.TB_naam_player1.Name = "TB_naam_player1";
+            this.TB_naam_player1.ReadOnly = true;
+            this.TB_naam_player1.Size = new System.Drawing.Size(100, 34);
+            this.TB_naam_player1.TabIndex = 1;
+            this.TB_naam_player1.Text = "Naam:";
+            this.TB_naam_player1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // PB_player1
+            // 
+            this.PB_player1.Image = ((System.Drawing.Image)(resources.GetObject("PB_player1.Image")));
+            this.PB_player1.Location = new System.Drawing.Point(6, 33);
+            this.PB_player1.Name = "PB_player1";
+            this.PB_player1.Size = new System.Drawing.Size(200, 300);
+            this.PB_player1.TabIndex = 0;
+            this.PB_player1.TabStop = false;
+            // 
+            // Player2
+            // 
+            this.Player2.Controls.Add(this.BoxItemRulings_player2);
+            this.Player2.Controls.Add(this.comboBox1);
+            this.Player2.Controls.Add(this.toolStrip2);
+            this.Player2.Controls.Add(this.TB_playerNaam2);
+            this.Player2.Controls.Add(this.TB_naam_player2);
+            this.Player2.Controls.Add(this.PB_player2);
+            this.Player2.Location = new System.Drawing.Point(788, 17);
+            this.Player2.Name = "Player2";
+            this.Player2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.Player2.Size = new System.Drawing.Size(550, 700);
+            this.Player2.TabIndex = 2;
+            this.Player2.TabStop = false;
+            this.Player2.Text = "Player 2";
+            // 
+            // TB_playerNaam2
+            // 
+            this.TB_playerNaam2.Location = new System.Drawing.Point(6, 33);
+            this.TB_playerNaam2.Name = "TB_playerNaam2";
+            this.TB_playerNaam2.ReadOnly = true;
+            this.TB_playerNaam2.Size = new System.Drawing.Size(226, 34);
+            this.TB_playerNaam2.TabIndex = 3;
+            // 
+            // TB_naam_player2
+            // 
+            this.TB_naam_player2.Location = new System.Drawing.Point(238, 33);
+            this.TB_naam_player2.Name = "TB_naam_player2";
+            this.TB_naam_player2.ReadOnly = true;
+            this.TB_naam_player2.Size = new System.Drawing.Size(100, 34);
+            this.TB_naam_player2.TabIndex = 2;
+            this.TB_naam_player2.Text = "Naam:";
+            this.TB_naam_player2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // PB_player2
+            // 
+            this.PB_player2.Image = ((System.Drawing.Image)(resources.GetObject("PB_player2.Image")));
+            this.PB_player2.Location = new System.Drawing.Point(344, 33);
+            this.PB_player2.Name = "PB_player2";
+            this.PB_player2.Size = new System.Drawing.Size(200, 300);
+            this.PB_player2.TabIndex = 1;
+            this.PB_player2.TabStop = false;
+            // 
+            // tSTB_player1_Stats
+            // 
+            this.tSTB_player1_Stats.Name = "tSTB_player1_Stats";
+            this.tSTB_player1_Stats.Size = new System.Drawing.Size(50, 22);
+            this.tSTB_player1_Stats.Text = "Stats";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // tSTB_player1_Buffs
+            // 
+            this.tSTB_player1_Buffs.Name = "tSTB_player1_Buffs";
+            this.tSTB_player1_Buffs.Size = new System.Drawing.Size(51, 22);
+            this.tSTB_player1_Buffs.Text = "Buffs";
+            // 
+            // toolStripSeparator2
+            // 
+            this.toolStripSeparator2.Name = "toolStripSeparator2";
+            this.toolStripSeparator2.Size = new System.Drawing.Size(6, 25);
+            // 
+            // tSTB_player1_Rules
+            // 
+            this.tSTB_player1_Rules.Name = "tSTB_player1_Rules";
+            this.tSTB_player1_Rules.Size = new System.Drawing.Size(52, 22);
+            this.tSTB_player1_Rules.Text = "Rules";
+            // 
+            // toolStrip2
+            // 
+            this.toolStrip2.Dock = System.Windows.Forms.DockStyle.None;
+            this.toolStrip2.Font = new System.Drawing.Font("Imperial One", 8.999999F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.toolStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripLabel1,
+            this.toolStripSeparator3,
+            this.toolStripLabel2,
+            this.toolStripSeparator4,
+            this.toolStripLabel3});
+            this.toolStrip2.Location = new System.Drawing.Point(163, 111);
+            this.toolStrip2.Name = "toolStrip2";
+            this.toolStrip2.Size = new System.Drawing.Size(177, 25);
+            this.toolStrip2.TabIndex = 4;
+            this.toolStrip2.Text = "TS_player1";
+            // 
+            // toolStripLabel1
+            // 
+            this.toolStripLabel1.Name = "toolStripLabel1";
+            this.toolStripLabel1.Size = new System.Drawing.Size(50, 22);
+            this.toolStripLabel1.Text = "Stats";
+            // 
+            // toolStripSeparator3
+            // 
+            this.toolStripSeparator3.Name = "toolStripSeparator3";
+            this.toolStripSeparator3.Size = new System.Drawing.Size(6, 25);
+            // 
+            // toolStripLabel2
+            // 
+            this.toolStripLabel2.Name = "toolStripLabel2";
+            this.toolStripLabel2.Size = new System.Drawing.Size(51, 22);
+            this.toolStripLabel2.Text = "Buffs";
+            // 
+            // toolStripSeparator4
+            // 
+            this.toolStripSeparator4.Name = "toolStripSeparator4";
+            this.toolStripSeparator4.Size = new System.Drawing.Size(6, 25);
+            // 
+            // toolStripLabel3
+            // 
+            this.toolStripLabel3.Name = "toolStripLabel3";
+            this.toolStripLabel3.Size = new System.Drawing.Size(52, 22);
+            this.toolStripLabel3.Text = "Rules";
+            // 
+            // CB_Modelselector_player1
+            // 
+            this.CB_Modelselector_player1.FormattingEnabled = true;
+            this.CB_Modelselector_player1.Location = new System.Drawing.Point(213, 81);
+            this.CB_Modelselector_player1.Name = "CB_Modelselector_player1";
+            this.CB_Modelselector_player1.Size = new System.Drawing.Size(259, 34);
+            this.CB_Modelselector_player1.TabIndex = 4;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(79, 73);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(259, 34);
+            this.comboBox1.TabIndex = 5;
+            // 
+            // BoxItemRulings_player2
+            // 
+            this.BoxItemRulings_player2.Location = new System.Drawing.Point(6, 133);
+            this.BoxItemRulings_player2.Multiline = true;
+            this.BoxItemRulings_player2.Name = "BoxItemRulings_player2";
+            this.BoxItemRulings_player2.ReadOnly = true;
+            this.BoxItemRulings_player2.Size = new System.Drawing.Size(332, 554);
+            this.BoxItemRulings_player2.TabIndex = 6;
+            this.BoxItemRulings_player2.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(213, 140);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(331, 554);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // MiniToLife
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 26F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1350, 729);
+            this.Controls.Add(this.Player2);
+            this.Controls.Add(this.Player1);
+            this.Controls.Add(this.PB_logo);
+            this.Font = new System.Drawing.Font("Imperial One", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(8, 6, 8, 6);
+            this.Name = "MiniToLife";
+            this.Text = "Mini-to-Life";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            ((System.ComponentModel.ISupportInitialize)(this.PB_logo)).EndInit();
+            this.Player1.ResumeLayout(false);
+            this.Player1.PerformLayout();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PB_player1)).EndInit();
+            this.Player2.ResumeLayout(false);
+            this.Player2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PB_player2)).EndInit();
+            this.toolStrip2.ResumeLayout(false);
+            this.toolStrip2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox PB_logo;
+        private System.Windows.Forms.GroupBox Player1;
+        private System.Windows.Forms.TextBox TB_playerNaam1;
+        private System.Windows.Forms.TextBox TB_naam_player1;
+        private System.Windows.Forms.PictureBox PB_player1;
+        private System.Windows.Forms.GroupBox Player2;
+        private System.Windows.Forms.TextBox TB_playerNaam2;
+        private System.Windows.Forms.TextBox TB_naam_player2;
+        private System.Windows.Forms.PictureBox PB_player2;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ComboBox CB_Modelselector_player1;
+        private System.Windows.Forms.ToolStripLabel tSTB_player1_Stats;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripLabel tSTB_player1_Buffs;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator2;
+        private System.Windows.Forms.ToolStripLabel tSTB_player1_Rules;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ToolStrip toolStrip2;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel1;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator3;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel2;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator4;
+        private System.Windows.Forms.ToolStripLabel toolStripLabel3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox BoxItemRulings_player2;
+    }
+}
+
